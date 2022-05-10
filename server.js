e1d325678f818d8f27d9a794823927d55ae20940
Jalen Moore <100977121+moorejalen2800@users.noreply.github.com>
2022-05-10 13:59:35 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,14 +7,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-
-mongoose.connect(process.env.MONGODB_URI ||
-    'mongodb://localhost/social-network',{
-        useFindAndModify: false,
-        useNewUrlParsen: true,
-        useUnifiedTopology: true
-    });
-
-    mongoose.set('debug',true);
-    app.use(require('./routes'));
+app.use(require('./routes'));
     app.listen(PORT,()=> console.log(`Connected to local host`));
+
+
+
+
