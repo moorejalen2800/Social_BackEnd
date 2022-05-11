3911061a2797872e9b88aaff15d417b0d6e95b96
Jalen Moore <100977121+moorejalen2800@users.noreply.github.com>
2022-05-10 23:09:57 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.status(404).send('Wrong Route');
+  return res.status(404).send('Wrong Route');
 });
 
 module.exports = router;
